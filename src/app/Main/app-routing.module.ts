7614134e8b15108db8b5bd613a333f7261484944
ahadddd <ahad.shehbaz19@gmail.com>
2023-06-16 18:48:23 +0500

    Routing configured

--- src/app/Main/app-routing.module.ts
+++ src/app/Main/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Home/Home.component';
+import { DetailsComponent } from './Members/details/details.component';
+import { ListComponent } from './Members/list/list.component';
+import { ListsComponent } from './Lists/Lists.component';
+import { MessagesComponent } from './Messages/Messages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'members', component: ListComponent },
+  { path: 'members/:id', component: DetailsComponent },
+  { path: 'lists', component: ListsComponent },
+  { path: 'messages', component: MessagesComponent },
+  { path: '**', component: HomeComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
